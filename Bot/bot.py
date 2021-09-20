4606ddf63282c3537fb9236cf21f0c123824bdfb
Andre <andre.slavescu@gmail.com>
2021-09-20 19:35:41 -0400

    bot code initial commit

---
+++ Bot/bot.py
@@ -0,0 +1,24 @@
+import discord
+import os
+from dotenv import load_dotenv
+from neuralintents import GenericAssistant
+import h5py
+
+chatbot = GenericAssistant('Bot/intents.json')
+chatbot.train_model()
+chatbot.save_model()
+
+print("Bot is running")
+
+client = discord.Client()
+
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+        
+    if message.content.startswith("$funbot"):
+        response = chatbot.request(message.content[7:])
+        await message.channel.send(response)
+
+client.run("ODc1MDUzODU1MTg3MDIxODM0.YRP7aA.YDlKNCU2Qcyfswq6breMYEihC_4")
